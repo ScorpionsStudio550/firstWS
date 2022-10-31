aaec4b0b9d526fb0841578276abb0318e0e4b3a6
Guilherme Delacroix <ScorpionsStudio550@gmail.com>
2022-10-30 23:30:25 -0300

    starting

---
+++ source/backend/source/main/app/java/br/com/scorpionsstudios/ping/configRest.java
@@ -0,0 +1,18 @@
+/**=======================================================================================================================
+ * ?                                                     ABOUT
+ * @author         :  Guilherme Delacroix
+ * @email          :  
+ * @repo           :  
+ * @createdOn      :  
+ * @description    :  
+ *=======================================================================================================================**/
+
+package br.com.scorpionsstudios.ping;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class configRest extends Application {
+
+}
